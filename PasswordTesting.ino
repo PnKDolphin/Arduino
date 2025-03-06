3100e82900d3a2bcadd57dae9cf39e269647fc8c
PnKDolphin <75462587+PnKDolphin@users.noreply.github.com>
2025-03-05 17:52:15 -0800

    Update PasswordTesting.ino

--- PasswordTesting.ino
+++ PasswordTesting.ino
@@ -1,4 +1,5 @@
-    #include <Keypad.h>
+  #include <Keypad.h>
+  #include <string.h>
 
     #define RED 50
     #define GREEN 51
@@ -8,7 +9,7 @@
     const byte COLS = 4;
 
     const char password[4] = "1234";
-    char attempt[4] = "";
+    char attempt[5] = "";
     int current_digit = 0;     
     int digits_in_key;
 
@@ -48,33 +49,65 @@
     void loop() 
     {
       char pressed_key = keypad.getKey();
-      
-      /*if(pressed_key && pressed_key != '#' && pressed_key != '*')
-      {
-        SetColour('b');
-        Serial.println("0-9 pressed");
-      }
-      else if(pressed_key == '#')
-      {
-        SetColour('g');
-        Serial.println("# pressed");
+      if(pressed_key) HandleInput(pressed_key);
 
-      }
-      else if(pressed_key == '*')
+
+    }
+
+   
+   
+    void HandleInput(char key)
+    {
+      switch(key)
       {
-        SetColour('r');
-        Serial.println("* pressed");
-      }*/
+        default:
+          attempt[current_digit] = key;
+          current_digit++;
+          Serial.println(attempt);
+          break;
+        case '#':
+          SubmitAttempt();
+          break;
+        case '*':
+          ResetPassword();
+          break;
+      }
+
+      if(current_digit == digits_in_key)
+      {
+        SubmitAttempt();
+      }
     }
 
     void SubmitAttempt()
     {
+      if(strcmp(attempt, password) == 0)
+      {
         SetColour('g');
+        attempt[5] = {};
+        current_digit = 0;
+        delay(2000);
+        SetColour('0');
+      }
+      else
+      {
+        SetColour('r');
+        attempt[5] = {};
+        current_digit = 0;
+        delay(200);
+        SetColour('0');
+        delay(200);
+        SetColour('r');
+        delay(200);
+        SetColour('0');
+      }
     }
 
     void ResetPassword()
     {
       Serial.println("Reset Password");
+      attempt[5] = {};
+      current_digit = 0;
     }
 
     void SetColour(char colour)
